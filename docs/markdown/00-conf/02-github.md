f3993e6e86576897ba0edeb32aa201bea716c62b
Theo GENEZ <genez.t@sfeir.com>
2025-01-02 14:27:25 +0100

    chore: init project with 3.0 sfeir school theme

---
+++ docs/markdown/00-conf/02-github.md
@@ -0,0 +1,11 @@
+# github
+
+Vous pouvez trouver le projet sur mon github pour le cloner en local
+
+[https://github.com/tgenez/terraform-basic-aws-gcp](https://github.com/tgenez/terraform-basic-aws-gcp)
+
+Il y a aussi un lien vers les slides publiques
+
+[https://tgenez.github.io/terraform-basic-aws-gcp/#/](https://tgenez.github.io/terraform-basic-aws-gcp/#/)
+
+
---
+++ docs/scripts/slides.js
@@ -0,0 +1,22 @@
+import { SfeirThemeInitializer } from '../web_modules/sfeir-school-theme/sfeir-school-theme.mjs';
+
+// One method per module
+function schoolSlides() {
+  return ['00-conf/00-TITLE.md', '00-conf/01-speaker-genez-t.md'];
+}
+
+function introSlides() {
+  return ['intro/00-TITLE.md'];
+}
+
+function formation() {
+  return [
+    //
+    ...schoolSlides(), //
+    ...introSlides(), //
+  ].map((slidePath) => {
+    return { path: slidePath };
+  });
+}
+
+SfeirThemeInitializer.init(formation);
